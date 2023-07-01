56d4fa48c908e1f0ad01885d61cc1f82fed965e7
20120619 <mtuong669@gmail.com>
2023-07-01 14:54:49 +0700

    fix merge

--- client/src/components/Booking/BookingByHotel.js
+++ client/src/components/Booking/BookingByHotel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { Button, Form, Input, InputNumber, DatePicker } from "antd";
 import { Card, Col } from "react-bootstrap";
 import axios from "axios";
@@ -6,7 +6,6 @@ import { API_URL } from "../../utils/constants";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { createRentalCard, createBill } from "../../redux/bookingApi";
-import formatDate from "../../utils/formatDate";
 
 const BookingByHotel = () => {
     const user = useSelector((state) => state.auth.user);
@@ -107,18 +106,16 @@ const BookingByHotel = () => {
         else setSearchResult({ number: "", type: "", capacity: "", price: "" });
     };
     const onSearch = async (values) => {
-        await searchRoom(values.note)
-        .then(async res  => {
-                const response = await axios.post(`${API_URL}/book/check-booking`, {
-                    arrivalDate: new Date(),
-                    numDays:1,
-                    roomId: searchResult._id,
-                });
-    
-                setIsDuplicate(response.data.isDuplicate);
-        })
+        await searchRoom(values.note).then(async (res) => {
+            const response = await axios.post(`${API_URL}/book/check-booking`, {
+                arrivalDate: new Date(),
+                numDays: 1,
+                roomId: searchResult._id,
+            });
+
+            setIsDuplicate(response.data.isDuplicate);
+        });
         setSearch(values.note);
-        
     };
 
     return (
@@ -151,122 +148,124 @@ const BookingByHotel = () => {
                     </Form.Item>
                 </Form>
             </div>
-            { searchResult ?
-            <Col className="my-3">
-                <Card>
-                    <Card.Header className="text-primary">Phiếu đặt phòng</Card.Header>
-                    <Card.Body>
-                        <p className="mb-2">
-                            {">"} Số phòng: {searchResult.number}
-                        </p>
-                        <p className="mb-2">
-                            {">"} Loại phòng: {searchResult.type}
-                        </p>
-                        <p className="mb-2">
-                            {">"} Sức chứa: {searchResult.capacity}
-                        </p>
-                        <p className="mb-2">
-                            {">"} Giá: {searchResult.price.toLocaleString("vi")} VNĐ
-                        </p>
-                        <Form
-                            {...layout}
-                            ref={formRef}
-                            name="control-ref"
-                            onFinish={onFinish}
-                            style={{
-                                maxWidth: 600,
-                            }}
-                        >
-                            <Form.Item
-                                name="name"
-                                label="Họ tên:"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
+            {searchResult ? (
+                <Col className="my-3">
+                    <Card>
+                        <Card.Header className="text-primary">Phiếu đặt phòng</Card.Header>
+                        <Card.Body>
+                            <p className="mb-2">
+                                {">"} Số phòng: {searchResult.number}
+                            </p>
+                            <p className="mb-2">
+                                {">"} Loại phòng: {searchResult.type}
+                            </p>
+                            <p className="mb-2">
+                                {">"} Sức chứa: {searchResult.capacity}
+                            </p>
+                            <p className="mb-2">
+                                {">"} Giá: {searchResult.price.toLocaleString("vi")} VNĐ
+                            </p>
+                            <Form
+                                {...layout}
+                                ref={formRef}
+                                name="control-ref"
+                                onFinish={onFinish}
+                                style={{
+                                    maxWidth: 600,
+                                }}
                             >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item
-                                name="Email"
-                                label="Email:"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item
-                                name="cccd"
-                                label="Căn cước công dân:"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item
-                                name="arrivalDay"
-                                label="Ngày check-in:"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                            >
-                                <DatePicker />
-                            </Form.Item>
-                            <Form.Item
-                                name="numDays"
-                                label="Số ngày ở:"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                            >
-                                <InputNumber />
-                            </Form.Item>
-                            {isCheckOut && (
-                                <>
-                                    <p className="mb-2">
-                                        {">"} Tổng hóa đơn: {isCheckOut}
-                                    </p>
-                                </>
-                            )}
-                            {isDuplicate ? (
-                                        <div className="border-top pt-3 pb-2">
-                                            <p className="text-danger text-center">
-                                                Khoảng thời gian này <br />
-                                                phòng <b>{searchResult?.number}</b> đã có người đặt !!
-                                            </p>
-                                        </div>
-                                    ):
-
-                            <Form.Item {...tailLayout}>
-                                <Button type="primary" htmlType="submit">
-                                    Tính tiền
-                                </Button>
-                                <span style={{ padding: "12px" }}></span>
-                                <Button
-                                    type="primary"
-                                    onClick={async () => {
-                                        await handleBooking();
-                                    }}
+                                <Form.Item
+                                    name="name"
+                                    label="Họ tên:"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
                                 >
-                                    Đặt phòng
-                                </Button>
-                            </Form.Item> }
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </Col> : <span>Sai mã phòng</span>
-            }
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    name="Email"
+                                    label="Email:"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    name="cccd"
+                                    label="Căn cước công dân:"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    name="arrivalDay"
+                                    label="Ngày check-in:"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <DatePicker />
+                                </Form.Item>
+                                <Form.Item
+                                    name="numDays"
+                                    label="Số ngày ở:"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <InputNumber />
+                                </Form.Item>
+                                {isCheckOut && (
+                                    <>
+                                        <p className="mb-2">
+                                            {">"} Tổng hóa đơn: {isCheckOut}
+                                        </p>
+                                    </>
+                                )}
+                                {isDuplicate ? (
+                                    <div className="border-top pt-3 pb-2">
+                                        <p className="text-danger text-center">
+                                            Khoảng thời gian này <br />
+                                            phòng <b>{searchResult?.number}</b> đã có người đặt !!
+                                        </p>
+                                    </div>
+                                ) : (
+                                    <Form.Item {...tailLayout}>
+                                        <Button type="primary" htmlType="submit">
+                                            Tính tiền
+                                        </Button>
+                                        <span style={{ padding: "12px" }}></span>
+                                        <Button
+                                            type="primary"
+                                            onClick={async () => {
+                                                await handleBooking();
+                                            }}
+                                        >
+                                            Đặt phòng
+                                        </Button>
+                                    </Form.Item>
+                                )}
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ) : (
+                <span>Sai mã phòng</span>
+            )}
         </div>
     );
 };
