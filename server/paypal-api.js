da1bdcbd7c989cf0c5bd9780540efd155311ddd0
20120541 <nhiyenphan99@gmail.com>
2023-07-01 10:07:04 +0700

    fix key

--- server/paypal-api.js
+++ server/paypal-api.js
@@ -2,11 +2,10 @@ const fetch = require("node-fetch");
 // import * as fetch from 'node-fetch';
 // const { CLIENT_ID, APP_SECRET } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
-const CLIENT_ID = "AWO48qyUitVQoS7Y_3Uiw4tnqzm5rpyDBcoO-AVJFDjXbIFZuWXZ-hZgPNpD9XuwU_KaTNXg-6rmrWnS"
-const APP_SECRET = "EGwmRbr6uelc8S_LCXsmPi7afTegKJjqQQxRl7N-Nt4bxhqtMMuEC0iM7M2EJB9UrQH9RCeRIPvnv5Z_"
 
 const   generateAccessToken = async () => {
-  const auth = Buffer.from("AWO48qyUitVQoS7Y_3Uiw4tnqzm5rpyDBcoO-AVJFDjXbIFZuWXZ-hZgPNpD9XuwU_KaTNXg-6rmrWnS" + ":" + "EGwmRbr6uelc8S_LCXsmPi7afTegKJjqQQxRl7N-Nt4bxhqtMMuEC0iM7M2EJB9UrQH9RCeRIPvnv5Z_").toString("base64");
+  // const auth = Buffer.from("AWO48qyUitVQoS7Y_3Uiw4tnqzm5rpyDBcoO-AVJFDjXbIFZuWXZ-hZgPNpD9XuwU_KaTNXg-6rmrWnS" + ":" + "EGwmRbr6uelc8S_LCXsmPi7afTegKJjqQQxRl7N-Nt4bxhqtMMuEC0iM7M2EJB9UrQH9RCeRIPvnv5Z_").toString("base64");
+  const auth = Buffer.from(process.env.CLIENT_ID + ":" + process.env.APP_SECRET).toString("base64");
   
   const response = await fetch(`${base}/v1/oauth2/token`, {
     method: "post",
